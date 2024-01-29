8a7521f76ebbb75060e264a4067b016a462a733f
Ameerkhan <110119104009@aalimec.ac.in>
2024-01-29 15:00:23 +0530

    Add project files.

---
+++ TopSpeed.Domain/Models/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TopSpeed.Domain.Common;
+
+namespace TopSpeed.Domain.Models
+{
+    public class Brand : BaseModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Established Year")]
+        public int EstablishedYear { get; set; }
+
+        [Display(Name = "Brand Logo")]
+        public string BrandLogo { get; set; }
+    }
+    
+}
